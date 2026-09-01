5f589ffbbf6520c1c42d031bc68d1785f983cf58
Yashraj Singh <singhyash12356@gmail.com>
2026-09-01 09:46:44 +0530

    Cloudinary api

--- src/app.js
+++ src/app.js
@@ -1,50 +1,36 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-require("dotenv").config();
-const cookieParser = require("cookie-parser");
-
-const authRouter = require("./modules/auth/auth.router");
 //const mongoSanitization = require("express-mongo-sanitize");
+//app.use(mongoSanitization()); 
+
+require("dotenv").config();
+const cookieParser = require("cookie-parser"); 
+const authRouter = require("./modules/auth/authRoute");
 const apiResponse = require("./utils/apiResponse");
 const notFound = require("./middlewares/notfound.middleware");
-const errorHandler = require("./middlewares/errorHandler.middleware");
-
-
+const errorHandler = require("./middlewares/errorHandler.middleware"); 
 const app = express();
+
 app.use(express.json());
 app.use(helmet());
 app.use(cors({origin : process.env.CORS_ORIGIN , credentials : true}));
 app.use(cookieParser());
-//app.use(mongoSanitization());
-
-
 app.use("/api/v1/auth", authRouter);
 app.use(notFound);
-app.use(errorHandler);
-app.get('/api/v1/health',(req,res)=> 
-    res.status(200).json(
-        apiResponse
-        (200,
-        { service:	'ecom-backend',
-            env:	process.env.NODE_ENV,
-             uptimeSeconds:	Math.round(process.uptime()),
-             timestamp:	new	Date().toISOString(), 
-            },
-            	'API is running'
-            )));
+app.use(errorHandler); 
+app.get('/api/v1/health', ( req, res ) => 
+    res.status(200).json(  apiResponse (200,
+        { 
+            service: 'ecom-backend',  
+            env: process.env.NODE_ENV,  
+            uptimeSeconds:	Math.round(process.uptime()),
+            timestamp:	new	Date().toISOString(), 
+        },
+    	    'API is running'
+        )
+    )
+);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 module.exports = app;
